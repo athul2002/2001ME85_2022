867db61478f3a8531616693d91197fdafc986169
Athul <athulmaliyakkal@gmail.com>
2022-09-13 21:20:12 +0530

    Found out Octant value and added into new column named Octant

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -12,8 +12,27 @@ df['W Avg'][0]=W_Avg
 df["U'=U - U avg"]=df['U']-df['U Avg'][0]
 df["V'=V - V avg"]=df['V']-df['V Avg'][0]
 df["W'=W - W avg"]=df['W']-df['W Avg'][0]
+df['Octant']=''
+for i in range(len(df)):
+    if df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]>0:
+        df["Octant"][i]=1
+    elif df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]<0:
+        df["Octant"][i]=-1
+    elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]>0:
+        df["Octant"][i]=2
+    elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]<0:
+        df["Octant"][i]=-2
+    elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]>0:
+        df["Octant"][i]=3
+    elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]<0:
+        df["Octant"][i]=-3
+    elif df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]>0:
+        df["Octant"][i]=4
+    elif df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]<0:
+        df["Octant"][i]=-4
 df.to_csv('octant_output.csv', index=False)  
 df.reset_index(drop=True)  
+
 #def octact_identification(mod=5000):
     
 
