6fc20bdb9a951c2fa8b3c40b1c46b2980d60e1dd
Athul <athulmaliyakkal@gmail.com>
2022-09-14 03:54:46 +0530

    Modified the code to avoid the warning messages
    The code was modified to resolve the warning messages that comes while running the code.

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,35 +1,37 @@
 import pandas as pd
 df = pd.read_csv("D:\\ONEDRIVE\\Documents\\GitHub\\2001ME85_2022\\tut01\\octant_input.csv")
+#def octact_identification(mod):
+
 U_Avg  = df['U'].mean()
 df['U Avg']=''
-df['U Avg'][0]=U_Avg
+df.at[0,'U Avg']=U_Avg
 V_Avg  = df['V'].mean()
 df['V Avg']=''
-df['V Avg'][0]=V_Avg
+df.at[0,'V Avg']=V_Avg
 W_Avg  = df['W'].mean()
 df['W Avg']=''
-df['W Avg'][0]=W_Avg
+df.at[0,'W Avg']=W_Avg
 df["U'=U - U avg"]=df['U']-df['U Avg'][0]
 df["V'=V - V avg"]=df['V']-df['V Avg'][0]
 df["W'=W - W avg"]=df['W']-df['W Avg'][0]
 df['Octant']=''
 for i in range(len(df)):
     if df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]>0:
-        df["Octant"][i]=1
+        df.at[i,"Octant"]=1
     elif df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]<0:
-        df["Octant"][i]=-1
+        df.at[i,"Octant"]=-1
     elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]>0:
-        df["Octant"][i]=2
+        df.at[i,"Octant"]=2
     elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]<0:
-        df["Octant"][i]=-2
+        df.at[i,"Octant"]=-2
     elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]>0:
-        df["Octant"][i]=3
+       df.at[i,"Octant"]=3
     elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]<0:
-        df["Octant"][i]=-3
+        df.at[i,"Octant"]=-3
     elif df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]>0:
-        df["Octant"][i]=4
+       df.at[i,"Octant"]=4
     elif df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]<0:
-        df["Octant"][i]=-4
+        df.at[i,"Octant"]=-4
 df['']=''
 df['Octant ID']=''
 df['1']=''
@@ -40,7 +42,7 @@ df['3']=''
 df['-3']=''
 df['4']=''
 df['-4']=''
-df['Octant ID'][0]="Octant Count"
+df.at[0,'Octant ID']="Octant Count"
 df.reset_index(drop=True) 
 count1=0
 count2=0
@@ -51,33 +53,33 @@ count22=0
 count33=0
 count44=0 
 for i in range(len(df)):
-    if df['Octant'][i]==1:
+    if df.at[i,'Octant'] == 1:
         count1+=1
-    elif df['Octant'][i]==-1:
+    elif df.at[i,'Octant']==-1:
         count11+=1
-    elif df['Octant'][i]==2:
+    elif df.at[i,'Octant']==2:
         count2+=1
-    elif df['Octant'][i]==-2:
+    elif df.at[i,'Octant']==-2:
         count22+=1
-    elif df['Octant'][i]==3:
+    elif df.at[i,'Octant']==3:
         count3+=1
-    elif df['Octant'][i]==-3:
+    elif df.at[i,'Octant']==-3:
         count33+=1
-    elif df['Octant'][i]==4:
+    elif df.at[i,'Octant']==4:
         count4+=1
-    elif df['Octant'][i]==-4:
+    elif df.at[i,'Octant']==-4:
         count44+=1
-df['1'][0]=count1
-df['-1'][0]=count11
-df['2'][0]=count2
-df['-2'][0]=count22
-df['3'][0]=count3
-df['-3'][0]=count33
-df['4'][0]=count4
-df['-4'][0]=count44
-df.to_csv('octant_output.csv', index=False)  
-#def octact_identification(mod=5000):
-    
+df.at[0,'1']=count1
+df.at[0,'-1']=count11
+df.at[0,'2']=count2
+df.at[0,'-2']=count22
+df.at[0,'3']=count3
+df.at[0,'-3']=count33
+df.at[0,'4']=count4
+df.at[0,'-4']=count44
+df.at[1,'']="User Input"
+#mod=input()  
 
-#mod=5000
 #octact_identification(mod)
+df.to_csv('octant_output.csv', index=False)  
+
