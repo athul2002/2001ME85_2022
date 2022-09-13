791ec172ecbc9fb173b47cf9bec3e2a58d651b29
Athul <athulmaliyakkal@gmail.com>
2022-09-13 19:45:20 +0530

    Imported pandas and read the csv file

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,6 +1,7 @@
-def factorial(n):
-    if(n<=1):
-        return 1
-    return n*factorial(n-1)
-n=int(input())
-print(factorial(n))
+import pandas as pd
+df = pd.read_csv("D:\\ONEDRIVE\\Documents\\GitHub\\2001ME85_2022\\tut01\\octant_input.csv")
+def octact_identification(mod=5000):
+    
+
+mod=5000
+octact_identification(mod)
