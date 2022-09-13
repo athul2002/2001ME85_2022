1c3e23776ebe3c010c5e039007fa519f23034b21
Athul <athulmaliyakkal@gmail.com>
2022-09-13 20:42:22 +0530

    Found out average and added it into a new output file
    Using pandas library, found out the average of U,V and W columns and added them into a new column named U Avg, V avg and W avg in octant_output.csv file.

--- tut01/tut01.py
+++ tut01/tut01.py
@@ -1,7 +1,18 @@
 import pandas as pd
 df = pd.read_csv("D:\\ONEDRIVE\\Documents\\GitHub\\2001ME85_2022\\tut01\\octant_input.csv")
-def octact_identification(mod=5000):
+U_Avg  = df['U'].mean()
+df['U Avg']=''
+df['U Avg'][0]=U_Avg
+V_Avg  = df['V'].mean()
+df['V Avg']=''
+df['V Avg'][0]=V_Avg
+W_Avg  = df['W'].mean()
+df['W Avg']=''
+df['W Avg'][0]=W_Avg
+df.to_csv('octant_output.csv', index=False)  
+df.reset_index(drop=True)  
+#def octact_identification(mod=5000):
     
 
-mod=5000
-octact_identification(mod)
+#mod=5000
+#octact_identification(mod)
