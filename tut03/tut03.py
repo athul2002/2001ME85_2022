1dd5bf5b56a2ef21d7f19633de33fbd28c4bc7de
Athul <athulmaliyakkal@gmail.com>
2022-09-30 22:40:49 +0530

    Found out octant value

---
+++ tut03/tut03.py
@@ -0,0 +1,60 @@
+from datetime import datetime
+start_time = datetime.now()
+
+#Help https://youtu.be/H37f_x4wAC0
+#def octant_longest_subsequence_count():
+###Code
+
+from platform import python_version
+ver = python_version()
+import pandas as pd
+#Reading the input csv file
+df = pd.read_excel("input_octant_longest_subsequence.xlsx")
+
+#Finding mean of U,V,W and made a columns to add that
+U_Avg  = df['U'].mean()
+df['U Avg']=''
+df.at[0,'U Avg']=U_Avg
+V_Avg  = df['V'].mean()
+df['V Avg']=''
+df.at[0,'V Avg']=V_Avg
+W_Avg  = df['W'].mean()
+df['W Avg']=''
+df.at[0,'W Avg']=W_Avg
+
+#Calculated the U',V' and W' values and added into the column
+df["U'=U - U avg"]=df['U']-df['U Avg'][0]
+df["V'=V - V avg"]=df['V']-df['V Avg'][0]
+df["W'=W - W avg"]=df['W']-df['W Avg'][0]
+
+#Calculated the octant values and added into Octant column
+df['Octant']=''
+for i in range(len(df)):
+    if df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]>0:
+        df.at[i,"Octant"]=1
+    elif df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]<0:
+        df.at[i,"Octant"]=-1
+    elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]>0:
+        df.at[i,"Octant"]=2
+    elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]<0:
+        df.at[i,"Octant"]=-2
+    elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]>0:
+       df.at[i,"Octant"]=3
+    elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]<0:
+        df.at[i,"Octant"]=-3
+    elif df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]>0:
+       df.at[i,"Octant"]=4
+    elif df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]<0:
+        df.at[i,"Octant"]=-4
+        
+if ver == "3.8.10":
+    print("Correct Version Installed")
+else:
+    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+df.to_excel('output_octant_transition_identify.xlsx', index=False)  
+#octant_longest_subsequence_count()
+
+#This shall be the last lines of the code.
+end_time = datetime.now()
+print('Duration of Program Execution: {}'.format(end_time - start_time))
+
