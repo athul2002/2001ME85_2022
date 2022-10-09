76bde02d6be89a3252ca5b6fff5d38bc31c10827
Athul <athulmaliyakkal@gmail.com>
2022-10-09 13:39:30 +0530

    Added try, except

--- tut03/tut03.py
+++ tut03/tut03.py
@@ -40,62 +40,71 @@ def octant_longest_subsequence_count():
                 count=0
         
         # Printing the values in coloumns of excel
-        df.at[j,'Subsequent']=maxi
-        df.at[j,'Count']=Subsequence_count
-            
-import pandas as pd
+        try:
+            df.at[j,'Longest Subsequence Length']=maxi
+            df.at[j,'Count']=Subsequence_count
+        except:
+            print("An error occured while printing Longest subsequence / Count ")
+            break
 from platform import python_version
-ver = python_version()
-#Reading the input csv file
-df = pd.read_excel("input_octant_longest_subsequence.xlsx")
+try:
+    import pandas as pd
+    ver = python_version()
 
-#Finding mean of U,V,W and made a columns to add that
-U_Avg  = df['U'].mean()
-df['U Avg']=''
-df.at[0,'U Avg']=U_Avg
-V_Avg  = df['V'].mean()
-df['V Avg']=''
-df.at[0,'V Avg']=V_Avg
-W_Avg  = df['W'].mean()
-df['W Avg']=''
-df.at[0,'W Avg']=W_Avg
+    try:
+            
+        #Reading the input csv file
+        df = pd.read_excel("input_octant_longest_subsequence.xlsx")
 
-#Calculated the U',V' and W' values and added into the column
-df["U'=U - U avg"]=df['U']-df['U Avg'][0]
-df["V'=V - V avg"]=df['V']-df['V Avg'][0]
-df["W'=W - W avg"]=df['W']-df['W Avg'][0]
+        #Finding mean of U,V,W and made a columns to add that
+        U_Avg  = df['U'].mean()
+        df['U Avg']=''
+        df.at[0,'U Avg']=U_Avg
+        V_Avg  = df['V'].mean()
+        df['V Avg']=''
+        df.at[0,'V Avg']=V_Avg
+        W_Avg  = df['W'].mean()
+        df['W Avg']=''
+        df.at[0,'W Avg']=W_Avg
 
-#Calculated the octant values and added into Octant column
-df['Octant']=''
-for i in range(len(df)):
-    if df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]>0:
-        df.at[i,"Octant"]=1
-    elif df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]<0:
-        df.at[i,"Octant"]=-1
-    elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]>0:
-        df.at[i,"Octant"]=2
-    elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]<0:
-        df.at[i,"Octant"]=-2
-    elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]>0:
-       df.at[i,"Octant"]=3
-    elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]<0:
-        df.at[i,"Octant"]=-3
-    elif df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]>0:
-       df.at[i,"Octant"]=4
-    elif df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]<0:
-        df.at[i,"Octant"]=-4
-df['']=''
-df['octant']=''
-df['Subsequent']=''
-df['Count']=''
-if ver == "3.8.10":
-    print("Correct Version Installed")
-else:
-    print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
-octant_longest_subsequence_count()
-df.to_excel('output_octant_longest_subsequence.xlsx', index=False)  
+        #Calculated the U',V' and W' values and added into the column
+        df["U'=U - U avg"]=df['U']-df['U Avg'][0]
+        df["V'=V - V avg"]=df['V']-df['V Avg'][0]
+        df["W'=W - W avg"]=df['W']-df['W Avg'][0]
 
-#This shall be the last lines of the code.
-end_time = datetime.now()
-print('Duration of Program Execution: {}'.format(end_time - start_time))
-
+        #Calculated the octant values and added into Octant column
+        df['Octant']=''
+        for i in range(len(df)):
+            if df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]>0:
+                df.at[i,"Octant"]=1
+            elif df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]<0:
+                df.at[i,"Octant"]=-1
+            elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]>0:
+                df.at[i,"Octant"]=2
+            elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]>0 and df["W'=W - W avg"][i]<0:
+                df.at[i,"Octant"]=-2
+            elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]>0:
+                df.at[i,"Octant"]=3
+            elif df["U'=U - U avg"][i]<0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]<0:
+                df.at[i,"Octant"]=-3
+            elif df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]>0:
+                df.at[i,"Octant"]=4
+            elif df["U'=U - U avg"][i]>0 and df["V'=V - V avg"][i]<0 and df["W'=W - W avg"][i]<0:
+                df.at[i,"Octant"]=-4
+        df['']=''
+        df['octant']=''
+        df['Longest Subsequence Length']=''
+        df['Count']=''
+        if ver == "3.8.10":
+            print("Correct Version Installed")
+        else:
+            print("Please install 3.8.10. Instruction are present in the GitHub Repo/Webmail. Url: https://pastebin.com/nvibxmjw")
+        octant_longest_subsequence_count()
+        df.to_excel('output_octant_longest_subsequence.xlsx', index=False)  
+    except FileNotFoundError:
+        print("Not able to open the file. Try later")
+    #This shall be the last lines of the code.
+    end_time = datetime.now()
+    print('Duration of Program Execution: {}'.format(end_time - start_time))
+except ImportError:
+    print("Not found module Pandas")
