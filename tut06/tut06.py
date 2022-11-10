42409fb221054c0bc20d75eab9e2954d754aec15
Athul <athulmaliyakkal@gmail.com>
2022-11-10 14:38:42 +0530

    Added Comments

--- tut06/tut06.py
+++ tut06/tut06.py
@@ -44,24 +44,35 @@ def send_mail():
         print("Unable to send mail. Check the credentials.")
 def attendance_report():
     try:
+        #creating a list of roll numbers
         roll_nums = [str(i) for i in df1['Roll No']]
+        #creating a set containing dates of all mondays and thursdays
         set_date=({datetime.strptime(str(i).split(" ")[0],"%d-%m-%Y").date() for i in df['Timestamp']  if datetime.strptime(str(i).split(" ")[0],"%d-%m-%Y").strftime('%a') in ['Mon','Thu']})
+        #converting the set of dates into a list 
         list_date=list(set_date)
+        #sorting the list containing dates
         list_date.sort()
+        #making a dictionary named attendance
+        #inside the dictionary, for each roll number another dictionary is made which contains the date.
+        #inisde the date dictionary, the value of real is added by 1 if present and similarly for invalid and duplicate
         attendance={rollno:{date:{'real':0,'invalid':0,'duplicate':0,} for date in list_date} for rollno in roll_nums}
         for i in range(len(df['Timestamp'])):
             d = (datetime.strptime(str(df['Timestamp'][i]), '%d-%m-%Y %H:%M')).date()
             rollno=(str(df['Attendance'][i])).split(" ")[0]
             if (datetime.strptime(str(df['Timestamp'][i]), '%d-%m-%Y %H:%M').weekday() == 0 or datetime.strptime(str(df['Timestamp'][i]), '%d-%m-%Y %H:%M').weekday() == 3):
                 if datetime.strptime(str(df['Timestamp'][i]), '%d-%m-%Y %H:%M').hour==14 or (datetime.strptime(str(df['Timestamp'][i]), '%d-%m-%Y %H:%M').hour==15 and datetime.strptime(str(df['Timestamp'][i]), '%d-%m-%Y %H:%M').minute==00):
+                    #when its a monday or thursday and between 2:00pm-3:00pm this loop runs
                     rollno=(str(df['Attendance'][i])).split(" ")[0]
                     if rollno == 'nan' or rollno not in roll_nums:
                         continue
+                    #if the real attendance of a particular roll number is zero for that date, real value is increased by 1
                     if attendance[rollno][d]['real']==0:
                         attendance[rollno][d]['real']+=1
                     else:
+                    #if real value is not zero, value of duplicate is added by 1
                         attendance[rollno][d]['duplicate']+=1
                 else:
+                    #when attendance is marked other than during lecture hour, invalid value is increased by 1
                     attendance[rollno][d]['invalid']+=1
             else:
                 continue 
@@ -75,28 +86,37 @@ def attendance_report():
             dfi.at[0,'Name']=df1['Name'][i]  
             count=1
             real_count=0
+            #total_real is for finding total real attendnace
             total_real=0
+            #total_duplicate is for finding total duplicate attendnace
             total_duplicate=0
+            #total_invalid is for finding total invalid attendnace
             total_invalid=0
             for date in list_date:
                 dfi.at[count,'Date']=date
+                #printing Total attendance one person marked on  a day as the sum of invalid, real and duplicate attendnace on that day
                 dfi.at[count,'Total Attendance']=attendance[df1['Roll No'][i]][date]['real']+attendance[df1['Roll No'][i]][date]['duplicate']+attendance[df1['Roll No'][i]][date]['invalid']
+                #printing the real attendnace count on day               
                 dfi.at[count,'Real']=attendance[df1['Roll No'][i]][date]['real']
                 total_real+=attendance[df1['Roll No'][i]][date]['real']
+                #printing count of duplictae attendnace on a day
                 dfi.at[count,'Duplicate']=attendance[df1['Roll No'][i]][date]['duplicate']
                 total_duplicate+=attendance[df1['Roll No'][i]][date]['duplicate']
+                #printing count of invalid attendance count
                 dfi.at[count,'Invalid']=attendance[df1['Roll No'][i]][date]['invalid']
                 total_invalid+=attendance[df1['Roll No'][i]][date]['invalid']
+                #printing 1 in absent column if real column value is 0
                 dfi.at[count,'Absent']=1-attendance[df1['Roll No'][i]][date]['real']
                 count+=1
                 dfc.at[i,'Roll']=df1['Roll No'][i]
                 dfc.at[i,'Name']=df1['Name'][i]
+
+                #marking present as P and absent and A            
                 if attendance[df1['Roll No'][i]][date]['real']==1:
                     dfc.at[i,date]='P'
                     real_count+=1
                 else:
                     dfc.at[i,date]='A'
-                
             dfc.at[i,'Actual Lecture Taken']=len(list_date)
             dfc.at[i,'Total Real']=real_count
             dfc.at[i,'Percentage']=round((real_count/len(list_date))*100,2)
@@ -121,13 +141,14 @@ try:
         dfc = pd.DataFrame()
         list_fake=[]
         list_actual=[]
-        roll_nums = [str(i) for i in df1['Roll No']]
         try:
+            #function call for making attendance report
             attendance_report()
         except:
             print("Function not defined.")
         dfc.to_excel('./output/attendance_report_consolidated.xlsx',index=False)
         try:
+            #function call for sending mail
             send_mail()
         except:
             print("Mail function not found.")
